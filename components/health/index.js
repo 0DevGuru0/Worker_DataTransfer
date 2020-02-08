10905670feadd4d6fbe92db8e5e6f4b51896063a
Afsan <af.hadafi@gmail.com>
2020-02-08 11:01:28 +0330

    2020/2/8

--- components/health/index.js
+++ components/health/index.js
@@ -1,7 +1,17 @@
+const col = require("chalk");
 const { connectToDBs, disconnectFromDBs } = require("../../database");
 
+let DBs = [];
 module.exports = () =>
   connectToDBs()
-    .then(() => console.log("databases health is audited successfully."))
+    .then(res => {
+      console.log(
+        `\n\t${col.bold.green("databases health is audited successfully.")}\n`
+      );
+      DBs.push(res);
+      return res;
+    })
     .catch(console.log)
-    .finally(() => disconnectFromDBs());
+    .finally(() => {
+      if (DBs.length > 0) disconnectFromDBs(DBs[0]);
+    });
--- containers/handler/provider/index.js
+++ containers/handler/provider/index.js
@@ -1,9 +1,7 @@
 const onlineVisitorsStore = require("./onlineVisitorsStore");
 const pageViewsStore = require("./pageViewsStore");
-// const pageViewsStore = require("./test");
 const visitorsStateStore = require("./visitorsStateStore");
 const usersStore = require("./usersStore");
-// const usersStore = require("./test");
 
 module.exports = {
   onlineVisitorsStore,
